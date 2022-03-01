ca2ab0f1e6d08e15433d1ae3b55f62d3904d0b88
abrarali <91776475+abrarali-official@users.noreply.github.com>
2022-03-01 16:50:31 +0500

    Container & Text

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,6 @@
+//1- First page | Scaffold 
+//2- Container & Text
+
 import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
@@ -12,9 +15,38 @@ class HomePage extends StatelessWidget {
   appBar:AppBar(
     title: Text("My app"),
   ),
-  body: Container(
-    child:Center( child: Text('Hi Flutter')),
-    ),
+  body: Center(child: 
+  Container(
+padding: const EdgeInsets.all(20),    
+alignment: Alignment.center,
+// color: Colors.teal,
+width: 100,
+height: 100,
+decoration: BoxDecoration(
+  // shape: BoxShape.circle,
+  borderRadius: BorderRadius.circular(15),
+  boxShadow: [BoxShadow(
+    color: Color.fromRGBO(189, 189, 189, 1),
+    blurRadius: 5,
+    offset: Offset(2.0, 10)
+  ),
+  ],
+  color: Colors.teal,
+  gradient: LinearGradient(colors: [
+Colors.red , Colors.pink , Colors.blue
+  ])
+),
+child: Text( 
+  "Box" ,
+  
+  style:TextStyle(
+  color:Colors.white,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+
+) ),
+  ),
+  ),
     );
   }
 }
