c975c425b4fa860045ac50ef65019ccad1edf8f9
abrarali <91776475+abrarali-official@users.noreply.github.com>
2022-03-02 19:45:03 +0500

    ch4

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -69,10 +69,98 @@
 //       );
 //   }
 // }
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(title: "First App", home: HomePage()));
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("My app"),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Align(
+//           // alignment: Alignment.bottomLeft,
+//           alignment: Alignment.center,
+//           child: Container(
+//             // width: MediaQuery.of(context).size.width,
+//             width: 200,
+//             height: 400,
+//             // height: MediaQuery.of(context).size.height,
+//             color: Colors.black,
+//             child: Column(
+//               children: [
+//                 Column(
+//                         // body: row(
+//                     mainAxisAlignment: MainAxisAlignment.start,
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: <Widget>[
+//                     Container(
+//                       padding: const EdgeInsets.all(20),
+//                       alignment: Alignment.center,
+//                       color: Colors.teal,
+//                       width: 100,
+//                       height: 100,
+//                       // decoration: BoxDecoration(
+//                       //     // shape: BoxShape.circle,
+//                       //     borderRadius: BorderRadius.circular(15),
+//                       //     boxShadow: [
+//                       //       BoxShadow(
+//                       //           color: Color.fromRGBO(189, 189, 189, 1),
+//                       //           blurRadius: 5,
+//                       //           offset: Offset(2.0, 10)),
+//                       //     ],
+//                       //     color: Colors.teal,
+//                       //     gradient: LinearGradient(
+//                       //         colors: [Colors.red, Colors.pink, Colors.blue])),
+//                       // child: Text("Box",
+//                       //     style: TextStyle(
+//                       //       color: Colors.white,
+//                       //       fontSize: 20,
+//                       //       fontWeight: FontWeight.bold,
+//                       //     )),
+//                     ),
+//                     Container(
+//                       padding: const EdgeInsets.all(20),
+//                       alignment: Alignment.center,
+//                       color: Colors.red,
+//                       width: 100,
+//                       height: 100,
+//                     ),
+//                     Container(
+//                       padding: const EdgeInsets.all(20),
+//                       alignment: Alignment.center,
+//                       color: Colors.blue,
+//                       width: 100,
+//                       height: 100,
+//                     ),
+//                   ]
+//                   ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//       );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "First App", home: HomePage()));
+  runApp(
+    MaterialApp(
+      title: "First App",
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch:Colors.red,
+      ),
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -80,72 +168,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My app"),
+        title: Text("Flutter Learning"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          // alignment: Alignment.bottomLeft,
-          alignment: Alignment.center,
-          child: Container(
-            // width: MediaQuery.of(context).size.width,
-            width: 200,
-            height: 400,
-            // height: MediaQuery.of(context).size.height,
-            color: Colors.black,
-            child: Column(
-              children: [
-                Column(
-                        // body: row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      alignment: Alignment.center,
-                      color: Colors.teal,
-                      width: 100,
-                      height: 100,
-                      // decoration: BoxDecoration(
-                      //     // shape: BoxShape.circle,
-                      //     borderRadius: BorderRadius.circular(15),
-                      //     boxShadow: [
-                      //       BoxShadow(
-                      //           color: Color.fromRGBO(189, 189, 189, 1),
-                      //           blurRadius: 5,
-                      //           offset: Offset(2.0, 10)),
-                      //     ],
-                      //     color: Colors.teal,
-                      //     gradient: LinearGradient(
-                      //         colors: [Colors.red, Colors.pink, Colors.blue])),
-                      // child: Text("Box",
-                      //     style: TextStyle(
-                      //       color: Colors.white,
-                      //       fontSize: 20,
-                      //       fontWeight: FontWeight.bold,
-                      //     )),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      alignment: Alignment.center,
-                      color: Colors.red,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      alignment: Alignment.center,
-                      color: Colors.blue,
-                      width: 100,
-                      height: 100,
-                    ),
-                  ]
-                  ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      );
+      body: Container(),
+    );
   }
 }
